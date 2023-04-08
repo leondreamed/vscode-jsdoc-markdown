57b0d75a486f70c4d297b0440ed8a27a98c498dc
Leon Si <leon@leondreamed.com>
2023-04-08 18:47:11 -0400

    chore: esm

--- scripts/test.js
+++ scripts/test.js
@@ -1,10 +1,11 @@
-const path = require('node:path')
-const { runTests } = require('@vscode/test-electron');
+import path from 'node:path'
+import { runTests } from '@vscode/test-electron';
+import { join } from 'desm'
 
-const extensionDevelopmentPath = path.resolve(__dirname, '..');
-const extensionTestsPath = path.resolve(__dirname, './test');
+const extensionDevelopmentPath = join(import.meta.url, '..');
+const extensionTestsPath = join(import.meta.url, '../test/index.js');
 
-runTests({
+await runTests({
 	extensionDevelopmentPath,
 	extensionTestsPath,
 });
